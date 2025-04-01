ab2320ccab76173b890ab5b3c6c218ce7a555b3c
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2025-04-01 19:42:40 -0300

    Adiciona classe do contato

--- js/form.js
+++ js/form.js
@@ -1,51 +1,71 @@
-
-// //class contato
-
-// class contato {
-    
-// }
-
-// function Post(form) {
-
-//   let data = new contato(form.elements.namedItem("nome").value,
-//             form.elements.namedItem("sobrenome").value, 
-//             form.elements.namedItem("email").value, 
-//             form.elements.namedItem("cpf").value, 
-//             form.elements.namedItem("telefone").value, 
-//             form.elements.namedItem("contato").value);
-  
-// }
-
-// function Enviar() {
-
-//     var nome = document.getElementById("nomeid");
-
-//     if (nome.value != "") {
-//         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-//     }
-
-// }
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-
-
-
-    function exibe_nome(event) {
-        event.preventDefault(); // Evita que o formulário seja enviado e a página recarregue
-
-        let nome = document.getElementById("nome").value;
-        let sobrenome = document.getElementById("sobrenome").value;
-
-        console.log("Nome:", nome);
-        console.log("Sobrenome:", sobrenome);
+class Contato {
+    constructor(nome, sobrenome, email, telefone, tipoContato, mensagem, data) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.telefone = telefone;
+        this.tipoContato = tipoContato;
+        this.mensagem = mensagem;
+        this.data = data;
     }
-    exibe_nome();
+}
+
+let mensage_Base = [];
+
+document.querySelector(".form").addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let tipoContato = document.getElementById("tipo_contato").value;
+    let mensagem = document.getElementById("mensagem").value;
+    let data = new Date().toLocaleString();
 
 
+    let contato = new Contato(nome, sobrenome, email, telefone, tipoContato, mensagem, data);
+    mensage_Base.push(contato);
+    console.log(mensage_Base);
+
+    nome.trim()
+    clear_Erro()
+    // alert(`Olá ${nome}, sua mensagem foi enviada!`);
+    if(is_Empty(nome)){
+        document.getElementById("error_Forms").innerHTML = "O nome é obrigatório!";
+        return
+    }
+    clear_Erro()
+    if(have_Special(nome)){
+        document.getElementById("error_Forms").innerHTML = "O nome não pode conter caracteres especiais!";
+        return
+    }
+    clear_Erro()
+    if(!have_Number(nome)){
+        document.getElementById("error_Forms").innerHTML = "O nome não pode conter número";
+        return
+    }
+
+    console.log(have_Special(nome));
+    
 });
+
+function clear_Erro() {
+    document.getElementById("error_Forms").innerHTML = " ";
+}
+
+function is_Empty(value) {
+    return value === ""
+}
+
+function have_Special(value){
+    const regexEspecial = /[!@#$%^&*(),.?":{}|<>]/;
+    return regexEspecial.test(value);
+}
+
+function have_Number(value) {
+    const regexEspecial = /^\d+$/;
+    return regexEspecial.test(value);
+}
+
 
